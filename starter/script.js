137ab0e651be09cb8cc081d4e73e262b1a46ab3d
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-07 00:17:18 -0300

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -190,6 +190,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
